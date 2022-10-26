76ca714e6b219d986fd3aa643f26ad11a3625da8
Christiaan Meijer <c.meijer@esciencecenter.nl>
2022-10-26 12:08:49 +0200

    refactor match cases to dataclasses

--- benchmarking/distance_benchmark.py
+++ benchmarking/distance_benchmark.py
@@ -1,4 +1,5 @@
 import time
+from dataclasses import dataclass
 from pathlib import Path
 
 import PIL.Image
@@ -13,38 +14,44 @@ from benchmarking.Config import Config, original_config_options
 from utils import ImageNetModel, load_img, plot_saliency_map_on_image, set_all_the_seeds
 
 
-def run_image_vs_image_experiment(case, config: Config, output_folder: Path):
-    match case:
-        case 'bee_vs_fly':
-            input_image_file_name = 'bee.jpg'
-            reference_image_file_name = 'fly.jpg'
-        case 'labradoodles':
-            input_image_file_name = 'labradoodle1.jpg'
-            reference_image_file_name = 'labradoodle2.jpg'
-        case 'dogcar_vs_car':
-            input_image_file_name = 'dogcar.jpg'
-            reference_image_file_name = 'car1.jpg'
-        case 'dogcar_vs_dog':
-            input_image_file_name = 'dogcar.jpg'
-            reference_image_file_name = 'labradoodle1.jpg'
-        case 'flower_vs_car':
-            input_image_file_name = 'flower.jpg'
-            reference_image_file_name = 'car1.jpg'
-        case 'car_vs_bike':
-            input_image_file_name = 'car2.png'
-            reference_image_file_name = 'bike.jpg'
-        case _:
-            print(f"{case} is not a valid case in this experiment.")
+@dataclass
+class ImageVsImageCase:
+    name: str
+    input_image_file_name: str
+    reference_image_file_name: str
 
+
+def run_image_vs_image_experiment(case: ImageVsImageCase, config: Config, output_folder: Path):
     model = ImageNetModel()
 
-    input_image_path = Path(__file__).parent.parent / 'data/images/' / input_image_file_name
-    reference_image_path = Path(__file__).parent.parent / 'data/images/' / reference_image_file_name
+    input_image_path = Path(__file__).parent.parent / 'data/images/' / case.input_image_file_name
+    reference_image_path = Path(__file__).parent.parent / 'data/images/' / case.reference_image_file_name
     input_image, input_arr = load_img(input_image_path, model.input_size)
     reference_image_file_name, reference_arr = load_img(reference_image_path, model.input_size)
     embedded_reference = model.run_on_batch(reference_arr)
 
-    run_and_analyse_explainer(case, config, embedded_reference, input_arr, input_image, model, output_folder)
+    run_and_analyse_explainer(case.name, config, embedded_reference, input_arr, input_image, model, output_folder)
+
+
+@dataclass
+class ImageCaptioningCase:
+    name: str
+    input_image_file_name: str
+    caption: str
+
+
+def run_image_captioning_experiment(case: ImageCaptioningCase, config: Config, output_folder: Path):
+    # See first example at https://github.com/openai/CLIP#usage
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load("ViT-B/32", device=device)
+
+    input_image_path = Path(__file__).parent.parent / 'data/images/' / case.input_image_file_name
+    input_image, input_arr = load_img(input_image_path, (224, 224))
+    text = clip.tokenize([case.caption]).to(device)
+    embedded_reference = model.encode_text(text).detach().numpy()
+
+    run_and_analyse_explainer(case.name, config, embedded_reference, input_arr, input_image, model, output_folder,
+                              preprocess_function=lambda x: [preprocess(PIL.Image.fromarray(e)) for e in x])
 
 
 def run_and_analyse_explainer(case, config, embedded_reference, input_arr, input_image, model, output_folder,
@@ -68,54 +75,6 @@ def run_and_analyse_explainer(case, config, embedded_reference, input_arr, input
     plt.savefig(output_folder / (case + '.png'))
 
 
-def run_image_captioning_experiment(case, config: Config, output_folder: Path):
-    match case:
-        case 'bee image wrt a bee sitting on a flower':
-            input_image_file_name = 'bee.jpg'
-            caption = "a bee sitting on a flower"
-        case 'bee image wrt a bee':
-            input_image_file_name = 'bee.jpg'
-            caption = "a bee"
-        case 'bee image wrt an image of a bee':
-            input_image_file_name = 'bee.jpg'
-            caption = "an image of a bee"
-        case 'bee image wrt a fly':
-            input_image_file_name = 'bee.jpg'
-            caption = "a fly"
-        case 'bee image wrt a flower':
-            input_image_file_name = 'bee.jpg'
-            caption = "a flower"
-        case 'labradoodles':
-            input_image_file_name = 'labradoodle1.jpg'
-            caption = 'a labradoodle'
-        case 'dogcar_vs_car':
-            input_image_file_name = 'dogcar.jpg'
-            caption = 'a car'
-        case 'dogcar_vs_dog':
-            input_image_file_name = 'dogcar.jpg'
-            caption = 'a dog'
-        case 'flower_vs_car':
-            input_image_file_name = 'flower.jpg'
-            caption = 'a car'
-        case 'car_vs_bicycle':
-            input_image_file_name = 'car2.png'
-            caption = 'a bicycle'
-        case _:
-            print(f"{case} is not a valid case in this experiment.")
-
-    # See first example at https://github.com/openai/CLIP#usage
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model, preprocess = clip.load("ViT-B/32", device=device)
-
-    input_image_path = Path(__file__).parent.parent / 'data/images/' / input_image_file_name
-    input_image, input_arr = load_img(input_image_path, (224, 224))
-    text = clip.tokenize([caption]).to(device)
-    embedded_reference = model.encode_text(text).detach().numpy()
-
-    run_and_analyse_explainer(case, config, embedded_reference, input_arr, input_image, model, output_folder,
-                              preprocess_function=lambda x: [preprocess(PIL.Image.fromarray(e)) for e in x])
-
-
 def run_benchmark(config, run_uid=None):
     if run_uid is None:
         run_uid = int(time.time())
@@ -125,24 +84,60 @@ def run_benchmark(config, run_uid=None):
     with open(output_folder / 'config.yml', 'w') as file:
         yaml.dump(config, file)
 
-    imagenet_cases = ['bee_vs_fly', 'labradoodles', 'dogcar_vs_car', 'dogcar_vs_dog', 'flower_vs_car', 'car_vs_bike']
-    imagenet_cases = []
+    imagenet_cases = [ImageVsImageCase(name='bee_vs_fly',
+                                       input_image_file_name='bee.jpg',
+                                       reference_image_file_name='fly.jpg'),
+                      ImageVsImageCase(name='labradoodles',
+                                       input_image_file_name='labradoodle1.jpg',
+                                       reference_image_file_name='labradoodle2.jpg'),
+                      ImageVsImageCase(name='dogcar_vs_car',
+                                       input_image_file_name='dogcar.jpg',
+                                       reference_image_file_name='car1.jpg'),
+                      ImageVsImageCase(name='dogcar_vs_dog',
+                                       input_image_file_name='dogcar.jpg',
+                                       reference_image_file_name='labradoodle1.jpg'),
+                      ImageVsImageCase(name='flower_vs_car',
+                                       input_image_file_name='flower.jpg',
+                                       reference_image_file_name='car1.jpg'),
+                      ImageVsImageCase(name='car_vs_bike',
+                                       input_image_file_name='car2.png',
+                                       reference_image_file_name='bike.jpg')]
+    # imagenet_cases = []
     for imagenet_case in imagenet_cases:
         run_image_vs_image_experiment(imagenet_case, config, run_uid)
 
-    image_captioning_cases = ['bee image wrt a bee sitting on a flower',
-                              'bee image wrt a bee',
-                              'bee image wrt an image of a bee',
-                              'bee image wrt a fly',
-                              'bee image wrt a flower',
-                              'labradoodles',
-                              'dogcar_vs_car',
-                              'dogcar_vs_dog',
-                              'flower_vs_car',
-                              'car_vs_bicycle', ]
-
-    image_captioning_cases = ['bee image wrt a bee']
-
+    image_captioning_cases = [
+        ImageCaptioningCase(name='bee image wrt a bee sitting on a flower',
+                            input_image_file_name='bee.jpg',
+                            caption="a bee sitting on a flower"),
+        ImageCaptioningCase(name='bee image wrt a bee',
+                            input_image_file_name='bee.jpg',
+                            caption="a bee"),
+        ImageCaptioningCase(name='bee image wrt an image of a bee',
+                            input_image_file_name='bee.jpg',
+                            caption="an image of a bee"),
+        ImageCaptioningCase(name='bee image wrt a fly',
+                            input_image_file_name='bee.jpg',
+                            caption="a fly"),
+        ImageCaptioningCase(name='bee image wrt a flower',
+                            input_image_file_name='bee.jpg',
+                            caption="a flower"),
+        ImageCaptioningCase(name='labradoodles',
+                            input_image_file_name='labradoodle1.jpg',
+                            caption='a labradoodle'),
+        ImageCaptioningCase(name='dogcar_vs_car',
+                            input_image_file_name='dogcar.jpg',
+                            caption='a car'),
+        ImageCaptioningCase(name='dogcar_vs_dog',
+                            input_image_file_name='dogcar.jpg',
+                            caption='a dog'),
+        ImageCaptioningCase(name='flower_vs_car',
+                            input_image_file_name='flower.jpg',
+                            caption='a car'),
+        ImageCaptioningCase(name='car_vs_bicycle',
+                            input_image_file_name='car2.png',
+                            caption='a bicycle')
+    ]
     for case in image_captioning_cases:
         run_image_captioning_experiment(case, config, output_folder)
 
