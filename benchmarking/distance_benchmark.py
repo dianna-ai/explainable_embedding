7c1c15dfca14c89d22dc4fce866e9fb90c063b20
E. G. Patrick Bos <egpbos@gmail.com>
2024-05-28 11:57:42 +0200

    rerun one-sided experiment (again) with now hopefully correct central values

--- benchmarking/distance_benchmark.py
+++ benchmarking/distance_benchmark.py
@@ -1,3 +1,4 @@
+import pathlib
 import time
 from dataclasses import dataclass
 from pathlib import Path
@@ -230,8 +231,17 @@ if __name__ == '__main__':
     # import dataclasses
     #run_benchmark(dataclasses.replace(test_config, number_of_masks=500))
 
-    for run_config in runs_20240206:
-        run_benchmark(run_config)
+    # run_uid = int(time.time())
+    # for run_config in runs_20240206:
+    #     run_benchmark(run_config, run_uid=run_uid)
 
+    # N.B.: below a hacky way to fix one (one-sided) experiment!
+    paths = Path('output').glob(f'{runs_20240206[0].experiment_name}_*17096*')
+
+    for ix, run_config in enumerate(runs_20240206):
+        path = paths[ix]
+        run_uid = str(path).split("_")[:-1]
+        print(run_uid)
+        # run_benchmark(run_config, run_uid=run_uid)
     # for run_config in runs_20240227_moar_features_moar_masks:
     #    run_benchmark(run_config, image_image_cases=slice(0, 1), image_caption_cases=slice(1, 4, 2))
